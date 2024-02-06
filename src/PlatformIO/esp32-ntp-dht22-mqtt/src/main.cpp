e82c53ebb7b8117e5be3556a2727c52f2f1f5e4d
samm002 <samuelsdjodis@gmail.com>
2024-02-06 15:56:08 +0800

    using mqtt to send dht data according to ntp time done

--- src/PlatformIO/esp32-ntp-dht22-mqtt/src/main.cpp
+++ src/PlatformIO/esp32-ntp-dht22-mqtt/src/main.cpp
@@ -1,13 +1,42 @@
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <WiFi.h>
+#include "time.h"
+#include <DHT.h>
+#include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include "WiFiClientSecure.h"
 #include "credentials.h"
 
+#define pinDATA SDA
+
+const char* ntpServer = "pool.ntp.org"; // NTP server
+const long  gmtOffset_sec = 3600 * 8; // Set Timezone to WITA (GMT +8)
+const int   daylightOffset_sec = 0;
+
+typedef struct DHT_Value {
+  float temperature;
+  float humidity;
+} DHT_value;
+
+
 WiFiClientSecure client;
-PubSubClient mqtt_client(client);  
+PubSubClient mqtt_client(client);
+
+// StaticJsonDocument<32> doc; -> deprecated format
+
+const size_t capacity = 32;
+DynamicJsonDocument doc(capacity);
+
+DHT dht22(pinDATA, DHT22); 
+
+String getCurrentTime(void);
+DHT_Value senseDHT22(void);
+void publishSensorData(float temperature, float humidity);
 
 void setup() {
   Serial.begin(115200);
-  delay(100); 
+  dht22.begin();
 
   Serial.print("Attempting to connect to SSID: ");
   Serial.println(ssid);
@@ -23,6 +52,11 @@ void setup() {
   Serial.print("Connected to ");
   Serial.println(ssid);
 
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.print("Initial Time : ");
+  Serial.print(getCurrentTime());
+  Serial.println("");
+
   //Set up the certificates and keys
   client.setCACert(CA_cert);          //Root CA certificate
   client.setCertificate(ESP_CA_cert); //for client verification if the require_certificate is set to true in the mosquitto broker config
@@ -31,16 +65,22 @@ void setup() {
   mqtt_client.setServer(mqtt_server, port);
 }
 
-void loop() {  
+void loop() { 
+
+  DHT_Value dhtValue = senseDHT22(); 
   Serial.println("\nStarting connection to server...");
-  //if you use password for Mosquitto broker
-  //if (client.connect("ESP32", mqtt_user , mqtt_pass)) {
-  //if you dont use password for Mosquitto broker
   if (mqtt_client.connect("ESP32")) {                       
     Serial.print("Connected, mqtt_client state: ");
     Serial.println(mqtt_client.state());
+
     //Publsih a demo message to topic LivingRoom/TEMPERATURE with a value of 25
-    mqtt_client.publish("LivingRoom/TEMPERATURE", "25");
+    mqtt_client.publish("ZettaHouse/TemperatureAndHumidity", "25");
+
+    if (!isnan(dhtValue.temperature) && !isnan(dhtValue.humidity)) {
+    Serial.print(getCurrentTime());
+    Serial.printf(" Temperature: %.2f Celcius and Humidity is %.2f%%\n", dhtValue.temperature, dhtValue.humidity);
+    publishSensorData(dhtValue.temperature,dhtValue.humidity);
+    }
   }
   else {
     Serial.println("Connected failed!  client state:");
@@ -51,4 +91,59 @@ void loop() {
     Serial.print(lastError);
   }
   delay(10000);
+}
+
+// void printLocalTime() {
+//   struct tm timeinfo;
+
+  // if(!getLocalTime(&timeinfo)){
+  //   Serial.println("Failed to obtain time");
+  //   return;
+  // }
+  
+//   // Output full time information :
+//   // Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+//   Serial.print("On ");
+//   Serial.print(&timeinfo, "%B %d %Y");
+//   Serial.print(" at ");
+//   Serial.print(&timeinfo, "%H:%M");
+// }
+
+String getCurrentTime(void) {
+  struct tm timeinfo;
+
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+  }
+
+  char currentTime[30];
+  strftime(currentTime, sizeof(currentTime), "%B %d %Y - %H:%M", &timeinfo);
+
+  return String(currentTime);
+}
+
+DHT_Value senseDHT22() {
+  DHT_Value dht_value;
+
+  dht_value.temperature = dht22.readTemperature();
+  dht_value.humidity = dht22.readHumidity();
+
+  if (!isnan(dht_value.temperature) && !isnan(dht_value.humidity)) {
+    return dht_value;
+  } else {
+    Serial.println("Failed to read from DHT sensor!");
+    dht_value.temperature = NAN;
+    dht_value.humidity = NAN;
+    return dht_value;
+  }
+}
+
+void publishSensorData(float temperature, float humidity) {
+  doc["time"] = getCurrentTime();
+  doc["temperature"] = String(temperature, 2);
+  doc["humidity"] = String(humidity, 2);
+  String output;
+  serializeJson(doc, output);
+  mqtt_client.publish("ZettaHouse/DHT22", output.c_str());
 }
